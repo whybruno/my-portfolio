aca0abb43560c3b837b77c9e49745fefd26fa3c4
whybruno.dev@gmail.com <whybruno.dev@gmail.com>
2024-02-17 14:32:50 +0000

    Deploy Testing 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={'/my-portfolio'}>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
