dfedc1f883f24a2a785ca198a96b6e474f1a5f8b
whybruno.dev@gmail.com <whybruno.dev@gmail.com>
2024-02-17 14:40:06 +0000

    Deploy Testing 1.3

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'my-portfolio',
-  build: {
-    outDir: 'build'
-  },
   plugins: [react()],
+  base: '/my-portfolio/'
 })
